89ec4ff074b6553a674ea6e43e3ce0063c50ebe2
Azizul Hasan <azizulhasan.webappick@gmail.com>
2023-03-06 23:28:32 +0600

    text to audio file build for production.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,7 @@
 const mix = require('laravel-mix');
 // const path = require('path')
 // const webpack = require('webpack')
-mix.js('src/dashboard/index.js', 'admin/js/text-to-audio-dashboard.js').react();
+// mix.js('src/dashboard/index.js', 'admin/js/text-to-audio-dashboard.js').react();
 
-// mix.js('admin/js/text-to-audio.js', 'admin/js/text-to-audio-build.js');
+mix.js('admin/js/text-to-audio.js', 'admin/js/text-to-audio-build.js');
 mix.webpackConfig({});
