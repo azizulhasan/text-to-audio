def6bba3059311914e7b6ee5cc1adbcf403bb5a2
Azizul Hasan <azizulhasan.webappick@gmail.com>
2024-02-18 14:36:22 +0600

    1.5.13

--- text-to-audio.php
+++ text-to-audio.php
@@ -14,8 +14,8 @@
  * @wordpress-plugin
  * Plugin Name:       Text To Speech TTS Accessibility
  * Plugin URI:        https://atlasaidev.com/
- * Description:       Add accessibility to WordPress site to read contents out loud in more than 20 languages.
- * Version:           1.5.12
+ * Description:       Add accessibility to WordPress site to read contents out loud in more than 51 languages.
+ * Version:           1.5.13
  * Author:            Atlas AiDev
  * Author URI:        http://atlasaidev.com/
  * License:           GPL-2.0+
@@ -117,7 +117,7 @@ class TTA_Init {
 
     public function __construct() {
         if (!defined('TEXT_TO_AUDIO_VERSION')) {
-            define('TEXT_TO_AUDIO_VERSION', apply_filters('tts_version', '1.5.12'));
+            define('TEXT_TO_AUDIO_VERSION', apply_filters('tts_version', '1.5.13'));
         }
 
         if (!defined('TEXT_TO_AUDIO_PLUGIN_NAME')) {
