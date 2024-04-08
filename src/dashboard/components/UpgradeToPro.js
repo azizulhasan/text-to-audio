4160ed9dd0147df98d452ccc7e86c5994ef6492a
aziulhasan <azizlhasan.cr@gmail.com>
2024-04-08 22:22:43 +0600

    1.5.28: feature notice added.

--- src/dashboard/components/UpgradeToPro.js
+++ src/dashboard/components/UpgradeToPro.js
@@ -9,21 +9,22 @@ export default function UpgradeToPro({ }) {
                 <div className="card-body">
                     <u className="list-group text-left">
                         <li className="list-group-item">
-                            Get Live Support for firts time Integration.
+                            Get Live Support for first time Integration.
                         </li>
                         <li className="list-group-item">
                             51 languages support in pro version.
                         </li>
                         <li className="list-group-item">
-                            14 Days money back.
+                            14 Days money back guarantee.
                         </li>
-                        <li className="list-group-item" >
+                        <li className="list-group-item">
                             <a target="_blank" href="https://wpml.org/"> WPML Plugin Support</a>
                         </li>
                         <li className="list-group-item">
-                            <a target='_blank' href="https://wordpress.org/plugins/gtranslate/">GTranslate Plugin Support</a>
+                            <a target='_blank' href="https://wordpress.org/plugins/gtranslate/">GTranslate Plugin
+                                Support</a>
                         </li>
-                        <li className="list-group-item" >
+                        <li className="list-group-item">
                             Download the audio file for offline listening.
                         </li>
                         <li className="list-group-item">
@@ -32,8 +33,21 @@ export default function UpgradeToPro({ }) {
                         <li className="list-group-item">
                             Multiple Audio Player Support.
                         </li>
+                        <li className="list-group-item">
+                            Include Content By CSS Selectors
+                        </li>
+                        <li className="list-group-item">
+                            Exclude Content By CSS Selectors
+                        </li>
+                        <li className="list-group-item">
+                            Exclude Tags To Speak
+                        </li>
+                        <li className="list-group-item">
+                            Exclude Texts To Speak
+                        </li>
                     </u>
-                    <a target='_blank' href="https://atlasaidev.com/text-to-speech-pro/" className="tta_btn btn-center text-center">Upgrade</a>
+                    <a target='_blank' href="https://atlasaidev.com/text-to-speech-pro/"
+                       className="tta_btn btn-center text-center">Upgrade</a>
                 </div>
             </div> : null
         }
