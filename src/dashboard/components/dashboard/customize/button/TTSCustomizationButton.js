4c65e58d603428d341e396e25792ea67b8981970
aziulhasan <azizlhasan.cr@gmail.com>
2024-08-18 20:54:39 +0600

    TTS-97:

--- src/dashboard/components/dashboard/customize/button/TTSCustomizationButton.js
+++ src/dashboard/components/dashboard/customize/button/TTSCustomizationButton.js
@@ -3,58 +3,14 @@ import { Form } from 'react-bootstrap';
 import { __ } from '@wordpress/i18n'
 
 export default function TTSCustomizationButton({ listeningBtnStyle, handleChange, buttonLists }) {
-    let gttsLanguages = {
-        "af": "Afrikaans",
-        "sq": "Albanian",
-        "ar": "Arabic",
-        "hy": "Armenian",
-        "ca": "Catalan",
-        "zh": "Chinese",
-        "zh-cn": "Chinese (Mandarin/China)",
-        "zh-tw": "Chinese (Mandarin/Taiwan)",
-        "zh-yue": "Chinese (Cantonese)",
-        "hr": "Croatian",
-        "cs": "Czech",
-        "da": "Danish",
-        "nl": "Dutch",
-        "en": "English",
-        "en-au": "English (Australia)",
-        "en-uk": "English (United Kingdom)",
-        "en-us": "English (United States)",
-        "eo": "Esperanto",
-        "fi": "Finnish",
-        "fr": "French",
-        "de": "German",
-        "el": "Greek",
-        "ht": "Haitian Creole",
-        "hi": "Hindi",
-        "hu": "Hungarian",
-        "is": "Icelandic",
-        "id": "Indonesian",
-        "it": "Italian",
-        "ja": "Japanese",
-        "ko": "Korean",
-        "la": "Latin",
-        "lv": "Latvian",
-        "mk": "Macedonian",
-        "no": "Norwegian",
-        "pl": "Polish",
-        "pt": "Portuguese",
-        "pt-br": "Portuguese (Brazil)",
-        "ro": "Romanian",
-        "ru": "Russian",
-        "sr": "Serbian",
-        "sk": "Slovak",
-        "es": "Spanish",
-        "es-es": "Spanish (Spain)",
-        "es-us": "Spanish (United States)",
-        "sw": "Swahili",
-        "sv": "Swedish",
-        "ta": "Tamil",
-        "th": "Thai",
-        "tr": "Turkish",
-        "vi": "Vietnamese",
-        "cy": "Welsh"
+    let buttonPositions = {
+        "before_content": "Before Content",
+        "after_content": "After Content",
+        // "top_fixed": "Top Fixed",
+        // "bottom_fixed": "Bottom Fixed (Pro)",
+        // "bottom_left": "Bottom Left",
+        // "bottom_right": "Bottom Right (Pro)",
+        // "bottom_center": "Bottom Center",
     }
 
     return (
@@ -81,30 +37,29 @@ export default function TTSCustomizationButton({ listeningBtnStyle, handleChange
                     })}
                 </Form.Select>
             </Form.Group>
-            {/* {
-                listeningBtnStyle?.buttonSettings?.id == 3 && <Form.Group>
-                    <Form.Label htmlFor='language'>
-                        {__('Select Language')}
-                    </Form.Label>
-                    <Form.Select
-                        onChange={handleChange}
-                        name='language'
-                        id='language'
-                        value={listeningBtnStyle?.buttonSettings?.language || 'en-us'}
-                        aria-label='Select Language'>
-                        <option disabled>
-                            {__('Select Language')}
-                        </option>
-                        {Object.keys(gttsLanguages).map((langKey, index) => {
-                            return (
-                                <option key={langKey} value={langKey}>
-                                    {gttsLanguages[langKey]}
-                                </option>
-                            );
-                        })}
-                    </Form.Select>
-                </Form.Group>
-            } */}
+            {/*Button Positions*/}
+            <Form.Group>
+                <Form.Label htmlFor='button_position'>
+                    {__('Select Button Position')}
+                </Form.Label>
+                <Form.Select
+                    onChange={handleChange}
+                    name='button_position'
+                    id='button_position'
+                    value={listeningBtnStyle?.buttonSettings?.button_position || 'before_content'}
+                    aria-label='Select Button Position'>
+                    <option disabled>
+                        {__('Select Button Position')}
+                    </option>
+                    {Object.keys(buttonPositions).map((langKey, index) => {
+                        return (
+                            <option key={langKey} value={langKey}>
+                                {buttonPositions[langKey]}
+                            </option>
+                        );
+                    })}
+                </Form.Select>
+            </Form.Group>
         </>
     )
 }
