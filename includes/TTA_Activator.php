e922decd7c21ff574cc883451f0f5227bd5f5d64
aziulhasan <azizlhasan.cr@gmail.com>
2024-09-29 12:56:55 +0600

    1.7.22

--- includes/TTA_Activator.php
+++ includes/TTA_Activator.php
@@ -68,7 +68,6 @@ class TTA_Activator {
 				'tta__settings_exclude_tags'                          => [],
 				"tta__settings_display_btn_icon"                      => true,
 				"tta__settings_exclude_post_ids"                      => [],
-				"tta__settings_display_button_if_user_logged_in"      => false,
 				'tta__settings_stop_auto_playing_after_switching_tab' => true,
 				'tta__settings_stop_floating_button'                  => true,
 				'tta__settings_exclude_categories'                    => [],
