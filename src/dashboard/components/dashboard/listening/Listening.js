0878288da35216593c4337933fafdd3fd09cbf3e
aziulhasan <azizlhasan.cr@gmail.com>
2025-01-23 19:47:48 +0600

    TTS-136

--- src/dashboard/components/dashboard/listening/Listening.js
+++ src/dashboard/components/dashboard/listening/Listening.js
@@ -35,7 +35,7 @@ export default function Listening() {
 
     const [listeningSettings, setListeningSettings] = useState({
         tta__listening_voice: 'Google UK English Female',
-        tta__listening_pitch: 2,
+        tta__listening_pitch: 1,
         tta__listening_rate: 1,
         tta__listening_volume: 1,
         tta__listening_lang: 'en-GB',
