12bfb1ff15d25194cd571e46ea52f28e734b6d2b
azizulhasan <azizulhasan.cr@gmail.com>
2025-08-21 16:21:58 +0600

    1.9.15

--- includes/helpers.php
+++ includes/helpers.php
@@ -142,6 +142,7 @@ function tta_get_button_content($atts, $is_block = false, $tag_content = '')
 
     $excerpt_sanitized = '';
     if (isset($settings['tta__settings_add_post_excerpt_to_read']) && $settings['tta__settings_add_post_excerpt_to_read']) {
+        remove_all_filters('get_the_excerpt');
         $excerpt = get_the_excerpt();
         $excerpt_sanitized = tta_clean_content($excerpt);
         $excerpt_sanitized = tta_should_add_delimiter($excerpt_sanitized, $sentence_delimiter);
